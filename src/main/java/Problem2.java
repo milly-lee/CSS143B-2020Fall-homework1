8879080f9d11a5d61ed1243cdf3db602819aa6ad
milly <milly123@uw.edu>
2020-10-06 20:36:20 -0700

    sort the first data

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,5 +3,13 @@ public class Problem2 {
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
+        for(int i = 0; i < data.length - 1; i++){
+            if(data[i] > data[i + 1]){
+               int oldData = data[i+1]; //save data before is replace
+               data[i+1] = data[i];
+               data[i] = oldData;
+            }
+
+        }
     }
 }
