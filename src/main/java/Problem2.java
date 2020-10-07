3bd1ed2fd8644661cabf71e6958c9aec529722cb
milly <milly123@uw.edu>
2020-10-06 21:09:11 -0700

    I think is finish

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,23 +1,18 @@
-import java.util.Arrays;
+
 
 public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
-        for(int i = 0; i < data.length - 1; i++){
-            if(data[i] > data[i + 1]){
-               int oldData = data[i+1]; //save data before is replace
-               data[i+1] = data[i];
-               data[i] = oldData;
+        for(int j = 0; j < data.length; j++) {  //repeat bubble for good amount of times(outter loop)
+            for(int i = 0; i < data.length - 1; i++){
+                if(data[i] > data[i + 1]){      //needs to move
+                    int oldData = data[i+1];    //save data before is replace
+                    data[i+1] = data[i];
+                    data[i]   = oldData;
+                }
             }
-
         }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{6, 9, 2, 3, 4, 5, 7, 8};
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
     }
 }
